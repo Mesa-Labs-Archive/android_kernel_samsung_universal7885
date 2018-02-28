df6d5689c20646062e1b629fb1221d63142bcd9f
mydongistiny <jaysonedson@gmail.com>
2018-02-28 18:26:47 +0100

    cpufreq: implement cultivation governor v1.5
    cpufreq: cultivation: bring in initial release
     -based off of caf 4.4 commits
     -uses per-cpu timers
     -use display_state for screen off timer
        with option to set different timer rate when screen off
     -improrted fastlane with threshold from blu_active
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    cpufreq: cultivation: update with a few new tuneables
    added in:
    go_lowspeed_load
    validate above_hispeed_delay
    check hispeed_freq is within the policy limits
    powersave_bias
    version bump to 1.5
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -534,6 +534,9 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CULTIVATION)
+extern struct cpufreq_governor cpufreq_gov_cultivation;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_cultivation)
 #endif
 
 /*********************************************************************
